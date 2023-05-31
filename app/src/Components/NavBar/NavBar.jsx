0f0af356c1cc72285922afc4ad85c6d4588093aa
Gopal <kc7198931@gmail.com>
2023-05-31 14:18:59 +0530

    set up structure

---
+++ app/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav className='black flex align-center justify-spaceBetween' style={{width:"100vw",height:"1.5rem"}}>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </nav>
+    )
+  }
+}
---
+++ app/src/Pages/App.jsx
@@ -0,0 +1,8 @@
+import NavBar from "../Components/NavBar/NavBar";
+import React from 'react'
+
+export default function App() {
+  return (
+    <NavBar />
+  )
+}
